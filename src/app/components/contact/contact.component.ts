8cbed155827cb233749fb5e0de0ec69f9edb8453
JulioAlb453 <fuliobeto18@gmail.com>
2024-12-03 01:29:29 -0600

    cambios de estilos en home (hero-section, contact, how-we-work)

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -4,13 +4,17 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
   constructor(private router: Router) {}
 
+  text: string = `En ONAT, transformamos cada aporte en alimentos para quienes más lo necesitan. 
+      Colaboramos con organizaciones comunitarias para brindar apoyo a personas en situación
+       de vulnerabilidad. Juntos, podemos hacer la diferencia y nutrir comunidades enteras.`;
+
   navigateTo(route: string) {
     this.router.navigateByUrl(route);
-    return false; 
+    return false;
   }
 }
